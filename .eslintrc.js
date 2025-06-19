7552b81292d4a4cfdc2d97d980ca92bbc55646b4
igu <igoralissoncn@gmail.com>
2025-06-19 15:30:49 -0300

    fix: improvements on lib + types

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
     node: true,
-    mocha: true
+    mocha: true,
   },
 
   parserOptions: {
@@ -12,31 +12,27 @@ module.exports = {
 
   extends: "eslint:recommended",
 
-  // For the full list of rules, see: http://eslint.org/docs/rules/
   rules: {
     complexity: [2, 55],
     "max-statements": [2, 115],
     "no-unreachable": 1,
     "no-useless-escape": 0,
-
     "no-console": 0,
-    // To flag presence of console.log without breaking linting:
-    //"no-console": ["warn", { allow: ["warn", "error"] }],
-
-    "require-jsdoc": ["error", {
-        require: {
-          FunctionDeclaration: true,
-          MethodDefinition: true,
-          ClassDeclaration: true,
-          ArrowFunctionExpression: false
-        }
-    }],
-    "valid-jsdoc": [2, {
-      requireReturnDescription: false,
-      requireReturn: false,
-      requireParamDescription: false,
-      requireReturnType: true
-    }],
+    "require-jsdoc": "off",
+    "valid-jsdoc": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "guard-for-in": 1,
   },
-}
+
+  overrides: [
+    {
+      files: ["libs/vis-timeline/**"],
+      rules: {
+        "require-jsdoc": "off",
+        "valid-jsdoc": "off",
+        "no-unused-vars": "off",
+        "no-prototype-builtins": "off",
+      },
+    },
+  ],
+};
